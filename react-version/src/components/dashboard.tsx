deaababce3ecc2bbe91e5801a7d5ef9ba242e132
BrianM0330 <BrianM0330@gmail.com>
2021-09-07 20:56:43 -0500

    added state

--- react-version/src/components/dashboard.tsx
+++ react-version/src/components/dashboard.tsx
@@ -1,5 +1,26 @@
-import * as React from "react";
+import { useState } from 'react'
+import { Calculator } from './Calculator/calculator'
 
+//Parent component, will control the state of all productivity components.
 export const Dashboard: React.FunctionComponent = () => {
-  return <div> yo </div>;
-};
+	const [calculator, setCalculator] = useState({
+		display: 0,
+		operations: [],
+	})
+
+	const [counter, setCounter] = useState({
+		count: 0,
+		totalOperations: 0,
+	})
+
+	const [todo, setTodo] = useState({
+		allTasks: [],
+		completedTasks: [],
+	})
+
+	return (
+		<div>
+			<Calculator />
+		</div>
+	)
+}
